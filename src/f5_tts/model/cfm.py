24a0099044db173fc03d260ff1937405f30b096f
nqbinh <binh@med2lab.com>
2024-12-30 21:17:15 +0700

    fix: error when calculate loss

--- src/f5_tts/model/cfm.py
+++ src/f5_tts/model/cfm.py
@@ -283,11 +283,6 @@ class CFM(nn.Module):
         # flow matching loss
         loss = F.mse_loss(pred, flow, reduction="none")
         # Apply mask to loss
-        expanded_audio_mask = audio_mask.unsqueeze(-1)  # Shape: (bs, len, 1)
-        loss = loss * expanded_audio_mask
         loss = loss[rand_span_mask]
-
-        mask_sum = expanded_audio_mask.sum()
-
-        loss = loss.sum() / mask_sum
+        loss = loss.mean()
         return loss, cond, pred
